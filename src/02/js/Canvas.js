02fbe7d239ac512c27a980390040becc146522d8
Shinya Abe <s-abe@details.co.jp>
2021-08-26 18:16:06 +0900

    Page02完成

--- src/02/js/Canvas.js
+++ src/02/js/Canvas.js
@@ -47,24 +47,32 @@ export default class Canvas {
     this.render();
     // console.log(performance);
 
-    // this.mesh;
-    this.targetX = 0;
-    this.targetY = 0;
-
-    this.nowX = 0;
-    this.nowY = 0;
-
-    this.vx = 0;
-    this.vy = 0;
-
-    this.power = 0.7;
-    this.ease = 0.1;
+    this.angle = [];
+    this.angleZ = [];
+    this.speed = 1;
+    this.speedZ = 3;
+    this.size = 350;
 
     this.init();
+    this.number = 36;
+
+    for (let i = 0; i < this.number; i++) {
+      this.angle[i] = (360 / this.number) * i;
+      this.angleZ[i] = (360 / this.number) * i;
+      if (i > 0) {
+        this.init();
+      }
+    }
+
+    this.hour = 0;
+    this.minutes = 0;
+    this.timeBuffer = (Math.PI / 180) * 10;
+
+    this.getTime();
   }
 
   init() {
-    const geo = new THREE.OctahedronGeometry(100, 1);
+    const geo = new THREE.OctahedronGeometry(25, 1);
     const line = new THREE.LineBasicMaterial({
       color: 0xffffff,
       transparent: true,
@@ -73,55 +81,84 @@ export default class Canvas {
     const mat = new THREE.MeshPhongMaterial({
       color: 0xaabbcc,
       emissive: 0x072534,
-      // side: THREE.DoubleSide,
       flatShading: true,
     });
-    // this.mesh = new THREE.LineSegments(geo, line);
-    this.mesh = new THREE.Mesh(geo, mat);
+    this.mesh = new THREE.LineSegments(geo, line);
+    // this.mesh = new THREE.Mesh(geo, mat);
     this.scene.add(this.mesh);
   }
 
-  mouseMoved(x, y) {
-    this.targetX = x - this.w / 2;
-    this.targetY = -y + this.h / 2;
+  mouseMoved(x, y) {}
+
+  getTime() {
+    const now = new Date();
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+    this.hour = hour >= 12 ? hour - 12 : hour;
+    this.minutes = minutes;
+  }
+
+  animation(mesh, id) {
+    this.getTime();
+    if (mesh) {
+      this.angle[id] += this.speed;
+      this.angleZ[id] += this.speedZ;
+      const radian = this.angle[id] * (Math.PI / 180);
+      const radianZ = this.angleZ[id] * (Math.PI / 180);
+      mesh.position.y = Math.cos(radian) * this.size;
+      mesh.position.x = Math.sin(radian) * this.size;
+
+      //　時間
+      if (
+        this.angle[id] % 360 <
+          (this.hour + 0.4 + this.minutes / 60) * (360 / 12) &&
+        this.angle[id] % 360 >
+          (this.hour - 0.4 + this.minutes / 60) * (360 / 12)
+      ) {
+        mesh.material.color.g = 0.4;
+        mesh.material.color.b = 0.4;
+      } else {
+        mesh.material.color.g = 1;
+        mesh.material.color.b = 1;
+      }
+      if (
+        this.angle[id] % 360 <
+          (this.hour + 0.1 + this.minutes / 60) * (360 / 12) &&
+        this.angle[id] % 360 >
+          (this.hour - 0.1 + this.minutes / 60) * (360 / 12)
+      ) {
+        mesh.material.color.g = 0;
+        mesh.material.color.b = 0;
+      }
+
+      // 分
+      if (
+        this.angle[id] % 360 < (this.minutes + 1.5) * (360 / 60) &&
+        this.angle[id] % 360 > (this.minutes - 1.5) * (360 / 60)
+      ) {
+        mesh.material.color.r = 0.4;
+        mesh.material.color.b = 0.4;
+      } else {
+        mesh.material.color.r = 1;
+        mesh.material.color.b = 1;
+      }
+      if (
+        this.angle[id] % 360 < (this.minutes + 0.5) * (360 / 60) &&
+        this.angle[id] % 360 > (this.minutes - 0.5) * (360 / 60)
+      ) {
+        mesh.material.color.r = 0;
+        mesh.material.color.b = 0;
+      }
+      mesh.position.z = Math.abs(Math.cos(radianZ) + Math.cos(radianZ)) * 50;
+    }
   }
 
   render() {
     requestAnimationFrame(() => {
       this.render();
     });
-    // console.log(this.mesh);
-    if (this.mesh) {
-      // 慣性
-      this.nowX += (this.targetX - this.nowX) * this.ease;
-      this.nowY += (this.targetY - this.nowY) * this.ease;
-
-      //バネ
-      // this.vx += (this.targetX - this.nowX) * this.power;
-      // this.vy += (this.targetY - this.nowY) * this.power;
-      // this.nowX += this.vx *= this.power;
-      // this.nowY += this.vy *= this.power;
-
-      const dx = this.targetX - this.nowX;
-      const dy = this.targetY - this.nowY;
-      const distance = Math.sqrt(dx * dx + dy * dy) / 1000;
-
-      this.mesh.position.x = this.nowX;
-      this.mesh.position.y = this.nowY;
-
-      // this.mesh.rotation.z += 0.01;
-
-      console.log(distance);
-      if (dx > 0) {
-        this.mesh.rotation.y += distance;
-      } else {
-        this.mesh.rotation.y -= distance;
-      }
-      if (dy < 0) {
-        this.mesh.rotation.x += distance;
-      } else {
-        this.mesh.rotation.x -= distance;
-      }
+    for (let j = 1; j < this.scene.children.length; j++) {
+      this.animation(this.scene.children[j], j - 1);
     }
 
     this.renderer.render(this.scene, this.camera);
